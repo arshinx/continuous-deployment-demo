d57346299565591438a32c99d43801470bbe993a
arshin <arshinx@users.noreply.github.com>
2019-01-24 15:24:16 -0800

    Project ref to GCP

--- e2e_test.py
+++ e2e_test.py
@@ -11,12 +11,10 @@
 import urllib2
 import logging
 
-HOST='http://continuous-deployment-python.appspot.com'
+HOST='http://travis-ci-aaa-test.appspot.com'
 
 # [START e2e]
 response = urllib2.urlopen("{}/get_author/ulysses".format(HOST))
 html = response.read()
 assert(html == "James Joyce")
 # [END e2e]
-
-
